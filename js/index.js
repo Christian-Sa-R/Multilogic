93ca251012b6992728af021b88975651165b6393
Christian-Sa-R <christian.sa.1507@gmail.com>
2026-06-20 20:15:54 -0300

    conclui a tarefa do filtro

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import "./scripts/geral.js";
+import "./scripts/aula.js";
 import "./scripts/listagemCursos.js";
 import "./scripts/listagemAulas.js";
 import "./scripts/matriculas.js";
--- js/scripts/geral.js
+++ js/scripts/geral.js
@@ -1,6 +1,21 @@
 // para utilizar o js no header e footer
+let temaEscuro = true;
+
+const body = document.querySelector("body");
 const botaoTema = document.querySelector("#header-botao-tema");
 
+function alternarTema() {
+    temaEscuro = !temaEscuro;
+
+    if (temaEscuro) {
+        body.classList.add("dark");
+        botaoTema.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    } else {
+        body.classList.remove("dark");
+        botaoTema.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    }
+}
+
 botaoTema.addEventListener("click", () => {
-    alert("clicou no tema");
+    alternarTema();
 });
