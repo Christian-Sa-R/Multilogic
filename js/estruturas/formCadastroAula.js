d63741f8ba04e5529ba432772ec3ab7b72468c74
Christian-Sa-R <christian.sa.1507@gmail.com>
2026-07-10 15:55:20 -0300

    tenho que corrigir o form de cadastro de aula

--- js/estruturas/formCadastroAula.js
+++ js/estruturas/formCadastroAula.js
@@ -6,8 +6,10 @@ import {
 import { criarBotaoFiltroBotoes } from "./criarBotoes.js";
 
 let botaoVoltar, botaoLimpar, botaoCancelar, botaoExcluir, botaoSalvar;
+let botaoNovoTexto, botaoNovoVideo, botaoExcluirConteudo;
 
 const camposForm = document.querySelectorAll(".form-input");
+const formConteudo = document.querySelector(".form-aula-conteudo");
 
 function botoesSuperiores() {
   linhaFiltrosBotoes.append(
@@ -59,12 +61,64 @@ function clickBotaoLimpar() {
   });
 }
 
+function botoesAdicionaisIniciais(edicao) {
+  if (document.querySelector("#botao-novo-texto")) {
+    botaoNovoTexto = document.querySelector("#botao-novo-texto");
+    botaoNovoTexto.addEventListener("click", () => {
+      formConteudo.append(adicionarTexto());
+    });
+  }
+  if (document.querySelector("#botao-novo-video")) {
+    botaoNovoVideo = document.querySelector("#botao-novo-video");
+    botaoNovoVideo.addEventListener("click", () => {
+      formConteudo.append(adicionarVideo());
+    });
+  }
+}
+
+function adicionarBotaoExcluir(elemento) {
+  const excluir = criarBotaoFiltroBotoes("", "Apagar");
+  excluir.type = "button";
+  excluir.addEventListener("click", () => {
+    elemento.remove();
+    excluir.remove();
+  });
+  return excluir;
+}
+
+function adicionarTexto() {
+  const input = document.createElement("textarea");
+  input.className = "form-input input-texto-aula";
+  input.addEventListener("input", () => {
+    input.style.height = "auto";
+    input.style.height = input.scrollHeight + "px";
+  });
+  input.after(adicionarBotaoExcluir(input));
+  return input;
+}
+
+function adicionarVideo() {
+  const video = document.createElement("video");
+  video.classList = "video-form";
+  video.controls = true;
+  video.src = "../videos/video_teste.mp4";
+  video.after(adicionarBotaoExcluir(video));
+  return video;
+}
+
+function carregarElementosExistentes() {
+  console.log("elementos existentes");
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   if (document.getElementById("form-cadastro-aula")) {
     if (sessionStorage.getItem("edicaoAula")) {
       carregarBotoes(true);
+      botoesAdicionaisIniciais();
+      carregarElementosExistentes();
     } else {
       carregarBotoes();
+      botoesAdicionaisIniciais();
     }
     //adicionarListennerFormCurso();
   }
